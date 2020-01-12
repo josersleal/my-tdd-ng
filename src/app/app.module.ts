e49f9d6616736ed6006c35297a91a3e48df8c161
JL <jose.r.s.leal@gmail.com>
2020-01-12 16:14:25 +0200

    data services generated at master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomesComponent } from './src/components/homes/homes.component';
 import { HeaderComponent } from './src/components/header/header.component';
+import { DataComponent } from './src/services/data/data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomesComponent,
-    HeaderComponent
+    HeaderComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule
