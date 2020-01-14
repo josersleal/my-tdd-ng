66e2e849acb02202be2c3e129b1ceadd37db92c5
Jose Leal <jose.r.s.leal@gmail.com>
2020-01-14 20:31:50 +0200

    homes with mock on service

--- src/app/src/components/homes/homes.component.spec.ts
+++ src/app/src/components/homes/homes.component.spec.ts
@@ -10,6 +10,7 @@ describe('HomesComponent', () => {
   let component: HomesComponent;
   let fixture: ComponentFixture<HomesComponent>;
   let dataService: DataService; // = jasmine.SpyObj<DataService>();
+  let homeData: Array<any>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,6 +26,24 @@ describe('HomesComponent', () => {
     fixture = TestBed.createComponent(HomesComponent);
     component = fixture.componentInstance;
     dataService = TestBed.get(DataService);
+    homeData = [
+      {
+        title: 'Home 1',
+        image: 'assets/listing.jpg',
+        location: 'New York'
+      },
+      {
+        title: 'Home 2',
+        image: 'assets/listing.jpg',
+        location: 'Boston'
+      },
+      {
+        title: 'Home 3',
+        image: 'assets/listing.jpg',
+        location: 'Chicago'
+      }
+    ];
+    spyOn(dataService, 'getHomes$').and.returnValue(of(homeData));
     fixture.detectChanges();
   });
   describe('When starting', () => {
@@ -35,54 +54,35 @@ describe('HomesComponent', () => {
 
     it('Then shows homes', () => {
       const elHome = fixture.nativeElement.querySelectorAll('[data-test="home"]');
-
       expect(elHome.length).toBe(3);
     });
-    fit('Then it shows homes (mocked)', () => {
-
-      const homeData = [
-        {
-          title: 'Home 1',
-          image: 'assets/listing.jpg',
-          location: 'New York'
-        },
-        {
-          title: 'Home 2',
-          image: 'assets/listing.jpg',
-          location: 'Boston'
-        },
-        {
-          title: 'Home 3',
-          image: 'assets/listing.jpg',
-          location: 'Chicago'
-        }
-      ];
-      spyOn(dataService, 'getHomes$').and.returnValue(of(homeData));
-
-      // fixture.detectChanges();
-      let result;
-      dataService.getHomes$()
-        .subscribe((res) => {
-          result = res;
-        });
-      expect(result).toEqual(homeData);
+    it('Then it shows homes (mocked)', () => {
+      /*
+            let result;
+            dataService.getHomes$()
+              .subscribe((res) => {
+                result = res;
+              });
+            expect(result).toEqual(homeData); */
       const elHome = fixture.nativeElement.querySelectorAll('[data-test="home"]');
       expect(dataService.getHomes$).toHaveBeenCalled();
-
-      // expect(elHome.length).toBe(3);
+      expect(elHome.length).toBe(3);
 
 
     });
+
     it('Then shows homes details', () => {
 
       const elHome = fixture.nativeElement.querySelector('[data-test="home"]'); // as HTMLElement;
-      console.log(JSON.stringify(elHome));
 
-      const title = elHome.querySelector('[data-text="title"]');
+
+      const title = elHome.querySelector('[data-test="title"]');
       expect(title.innerText).toContain('Home 1');
-      const image = elHome.querySelector('[data-text="image"]');
-      expect(image.innerText).toContain('listing1.jpg');
-      const location = elHome.querySelector('[data-text="location"]');
+      const image = elHome.querySelector('[data-test="image"]');
+      expect(image).toBeTruthy();
+      const location = elHome.querySelector('[data-test="location"]');
+      debugger;
+
       expect(location.innerText).toContain('New York');
     });
 
